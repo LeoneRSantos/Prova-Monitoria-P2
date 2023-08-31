30b7007d0549b95182734c4fc42393b278163e76
Leone Rodrigues Santos <leonnerodriguez1@gmail.com>
2023-08-30 21:07:26 -0300

    Refatoração em Main

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+package src;
+
+import src.models.Pizza;
+import src.models.PizzaAdicionada;
+import src.models.PizzaPadrao;
+
+public class Main {
+    public static void imprimirInformacoes(Pizza pizza) {
+        System.out.print("\n\nPizza: " + pizza.informarNome() + ", ingredientes: [" + pizza.retornarIngredientes()
+                + "], calorias: " + pizza.informarCalorias() + ", preço: " + pizza.informarPreco() + "\n");
+
+    }
+
+    public static void main(String[] args) { 
+        Pizza p = new PizzaPadrao("Marguerita", 1104, 10.00, "tomate, queijo"); 
+        imprimirInformacoes(p);
+
+        p = new PizzaAdicionada(p, "Salame"); 
+        imprimirInformacoes(p);
+
+    }
+}
